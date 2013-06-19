1a8c2a09390dfe8cf96dfc13ba169110e8e31a0c
Jarad Niemi <niemi@iastate.edu>
2013-06-19 08:33:02 -0500

    addded map of site locations

--- R/sites.R
+++ R/sites.R
@@ -1,3 +1,6 @@
+# If you are running this outside of the Makefile, you should be
+# one directory below the R directory.
+
 source("R/common/functions.R")
 
 d = read.csv("data/mnfb.csv")
@@ -16,7 +19,7 @@ library(xtable)
 tab = xtable(sites[dups,], 
              caption="Sites with duplicated or missing locations",
              label="tab:duplicated-sites")
-print(tab, file="duplicated-sites.tex", include.rownames=FALSE)
+print(tab, file="tables/duplicated-sites.tex", include.rownames=FALSE)
 rm(dups)
 
 
@@ -24,13 +27,13 @@ rm(dups)
 tab = xtable(as.data.frame(with(sites, table(forest)), responseName="number of sites"),
              caption="Number of sites in each national forest",
              label="tab:number-of-sites-in-forest")
-print(tab, file="number-of-sites-in-forest.tex", include.rownames=FALSE)
+print(tab, file="tables/number-of-sites-in-forest.tex", include.rownames=FALSE)
 
 
 # Make a map of the sites
 # This should be replaced with a real map using maptools or something similar
 library(ggplot2)
-pdf("site-map.pdf")
+pdf("figs/site-map.pdf")
 qplot(x=X_COORD, y=Y_COORD, data=sites, color=forest)
 dev.off()
 
