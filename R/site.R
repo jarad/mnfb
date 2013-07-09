fe0a3a3af92db68f6ea565b7d96b44bf9d07408c
Jarad Niemi <niemi@iastate.edu>
2013-07-09 12:50:48 -0500

    added plot for site-year surveys

--- R/site.R
+++ R/site.R
@@ -1,6 +1,10 @@
 # If you are running this outside of the Makefile, you should be
 # one directory below the R directory.
 
+
+library(plyr)
+library(ggplot2)
+
 source("R/common/functions.R")
 
 d = read.csv("data/location.csv")
@@ -45,12 +49,49 @@ dev.off()
 ############################
 # Sites 
 d2 = read.csv("data/site.csv")
+d2 = join(d2, d[,c("site","forest")], by = "site")
+d2 = d2[d2$forest %in% forest3,]
 
-library(plyr)
-f = ddply(d2, "site", summarise, number=length(year))
-f = join(f, d[,c("site","forest")], by = "site")
-ggplot(na.omit(f), aes(x=number))+geom_histogram(binwidth=1)+facet_wrap(~forest)
+f = ddply(d2, "site", summarise, number=length(year), forest=unique(forest))
+#ggplot(na.omit(f), aes(x=number))+geom_histogram(binwidth=1)+facet_wrap(~forest)
 
+
+# Image of which sites were surveyed in what years
+siteyear = function(d) {
+  sy = unique(d[,c("site","year")])
+  sy$site = as.factor(sy$site)
+  sy$year = as.factor(sy$year)
+
+  nsites = nlevels(sy$site)
+  nyears = nlevels(sy$year)
+
+  surveyed = matrix(0, nsites, nyears)
+  for (i in 1:nyears) {
+    sv = as.numeric(sy$site[sy$year==levels(sy$year)[i]])
+    surveyed[sv,i] = 1
+  }
+  colnames(surveyed) = levels(sy$year)
+  rownames(surveyed) = levels(sy$site)
+  
+  return(surveyed)
+}
+
+sy = dlply(d2, "forest", siteyear)
+
+for(i in 1:length(sy)){ attr(sy[[i]],"name") <- names(sy)[i] }
+
+
+site_image = function(x) {
+  n = rowSums(x)
+  image(as.numeric(colnames(x)), 1:length(n), t(x[order(n),]), 
+        ylab="Site", xlab="Year",
+        main=attr(x, "name"), col=c("white","black"))
+}
+
+pdf("figs/site-siteyear.pdf", width=6, height=4)
+par(mfrow=c(1,3), mar=c(4,4,2,0)+.1)
+l_ply(sy, site_image)
+dev.off()
 
 
 
