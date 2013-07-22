303459f4d0af44fab6d63742de5daccffcef83d0
Adam Martin-Schwarze <adamms@iastate.edu>
2013-07-22 09:42:16 -0500

    Models currently reflect exploration of stand, site, key random effects

--- R/common/models.R
+++ R/common/models.R
@@ -6,12 +6,33 @@ i=1
 
 # Models to Run
 
-z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|obsyr) + (1|standunique) + (1|site), data=birds, family=poisson)
-nms[i] <- c("fsdr / none")
+z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|standunique) + (1|site) + (1|key), data=birds, family=poisson)
+nms[i] <- c("stand, site, key")
 
 i = i+1
-z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|obsyr) + (1|site), data=birds, family=poisson)
-nms[i] <- c("fsdr / no stand")
+z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|site) + (1|key), data=birds, family=poisson)
+nms[i] <- c("site, key")
 
+i = i+1
+z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|key), data=birds, family=poisson)
+nms[i] <- c("key")
 
+i = i+1
+z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|standunique) + (1|site), data=birds, family=poisson)
+nms[i] <- c("stand, site")
 
+i = i+1
+z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|site), data=birds, family=poisson)
+nms[i] <- c("site")
+
+i = i+1
+z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|standunique), data=birds, family=poisson)
+nms[i] <- c("stand")
+
+i = i+1
+z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|standunique) + (1|key), data=birds, family=poisson)
+nms[i] <- c("stand, key")
+
+i = i+1
+z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr), data=birds, family=poisson)
+nms[i] <- c("none")
