16a1560913d5bfbbc542e9f3ca2a8d14572a2426
Jarad Niemi <niemi@iastate.edu>
2013-07-24 07:55:45 -0500

    initial try for tryCatch

--- R/common/models.R
+++ R/common/models.R
@@ -1,13 +1,32 @@
 require(lme4)
 
+myTryCatch = function(..., species, fornum, nms) {
+  tryCatch(...,
+    warning = function(w) {
+      # warning-handler-code
+      print(paste(species,fornum,nms,w,"\n"))
+    }, 
+    error = function(e) {
+      # error-handler-code
+      print(paste(species, fornum, nms, w,"\n"))
+    }, finally = {
+      # cleanup-code
+      print(paste(species, fornum, nms, "Success\n"))
+    })
+}
+
+
 # Establishing index and storage variables
 z <- list(); nms <- c()
 i=1
 
 # Models to Run
 
-z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|standunique) + (1|site) + (1|key), data=birds, family=poisson)
+
 nms[i] <- c("stand, site, key")
+myTryCatch(
+  z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|standunique) + (1|site) + (1|key), data=birds, family=poisson),
+  species, fornum, nms)
 
 i = i+1
 z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|site) + (1|key), data=birds, family=poisson)
