d2ceb45fa8d740b2150f3f45af2f0c6aeb4f032a
Jarad Niemi <niemi@iastate.edu>
2013-07-24 08:12:36 -0500

    tryCatch prints out errors

--- R/common/models.R
+++ R/common/models.R
@@ -1,17 +1,16 @@
 require(lme4)
 
-myTryCatch = function(..., species, fornum, nms) {
+myTryCatch = function(species, fornum, nms, ...) {
   tryCatch(...,
     warning = function(w) {
       # warning-handler-code
-      print(paste(species,fornum,nms,w,"\n"))
+      print(paste(species, fornum, nms, w))
     }, 
     error = function(e) {
       # error-handler-code
-      print(paste(species, fornum, nms, w,"\n"))
+      print(paste(species, fornum, nms, e))
     }, finally = {
       # cleanup-code
-      print(paste(species, fornum, nms, "Success\n"))
     })
 }
 
@@ -24,17 +23,17 @@ i=1
 
 
 nms[i] <- c("stand, site, key")
-myTryCatch(
-  z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|standunique) + (1|site) + (1|key), data=birds, family=poisson),
-  species, fornum, nms)
+myTryCatch(species, fornum, nms[i],
+  z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|standunique) + (1|site) + (1|key), data=birds, family=poisson))
 
 i = i+1
-z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|site) + (1|key), data=birds, family=poisson)
 nms[i] <- c("site, key")
+myTryCatch(species, fornum, nms[i], 
+  z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|site) + (1|key), data=birds, family=poisson))
 
 i = i+1
-z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|key), data=birds, family=poisson)
 nms[i] <- c("key")
+z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|key), data=birds, family=poisson)
 
 i = i+1
 z[i] <- glmer(N ~ fsdr + wind + noise + sky + year + year2 + jd + jd2 + time + time2 + temp + temp2 + siteorigyear + (1|yearf) + (1|obs) + (1|obsyr) + (1|standunique) + (1|site), data=birds, family=poisson)
